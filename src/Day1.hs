c0b7c39d89b998ef741a6b7637d7699211a98545
meck <johan@meck.se>
2019-12-01 15:38:25 +0100

    golf day1

--- src/Day1.hs
+++ src/Day1.hs
@@ -3,11 +3,11 @@ module Day1 (day01a, day01b) where
 fuel :: Int -> Int
 fuel x = x `div` 3 - 2
 
-fuel' :: Int -> Int
-fuel' = sum . takeWhile (> 0) . iterate fuel . fuel
+fuel' :: Int -> [Int]
+fuel' = takeWhile (> 0) . iterate fuel . fuel
 
 day01a :: String -> String
 day01a = show . sum . fmap (fuel . read) . lines
 
 day01b :: String -> String
-day01b = show . sum . fmap (fuel' . read) . lines
+day01b = show . sum . (fuel' . read =<<) . lines
