8319ef3b460c9fd5e8592e870920ba849f3344af
meck <johan@meck.se>
2019-12-01 09:08:12 +0100

    solve Day1

--- src/Day1.hs
+++ src/Day1.hs
@@ -1,8 +1,15 @@
 module Day1 (day01a, day01b) where
 
+fuel :: Int -> Int
+fuel x = x `div` 3 - 2
+
+fuel' :: Int -> Int
+fuel' x | f <= 0    = 0
+        | otherwise = f + fuel' f
+    where f = fuel x
 
 day01a :: String -> String
-day01a = id
+day01a = show . sum . fmap (fuel . read) . lines
 
 day01b :: String -> String
-day01b = id
+day01b = show . sum . fmap (fuel' . read) . lines
