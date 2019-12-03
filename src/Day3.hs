811bf10e91d738ecbc9753b06f54229f383c42e4
meck <johan@meck.se>
2019-12-03 13:54:58 +0100

    Day 3

--- src/Day3.hs
+++ src/Day3.hs
@@ -1,8 +1,52 @@
 module Day3 (day03a, day03b) where
 
+import           Data.Map                       ( Map )
+import qualified Data.Map                      as M
+import           Data.List                      ( scanl' )
+import           Data.List.Split                ( splitOn )
+import           Util                           ( manhaDist )
+
+type Cord = (Int, Int)
+
+origin :: Cord
+origin = (0, 0)
+
+data Direction = U | L | D | R
+
+expandStep :: String -> [Direction]
+expandStep (d : lenStr) = case d of
+    'U' -> rep U
+    'L' -> rep L
+    'D' -> rep D
+    'R' -> rep R
+    _   -> error "Bad Input"
+    where rep = replicate (read lenStr)
+expandStep _ = error "Bad Input"
+
+move :: Cord -> Direction -> Cord
+move (x, y) d = case d of
+    U -> (succ x, y)
+    L -> (x, pred y)
+    D -> (pred x, y)
+    R -> (x, succ y)
+
+makeWire :: [String] -> Map Cord Int
+makeWire steps = M.fromListWithKey (const min) $ zip cs [1 ..]
+    where cs = tail $ scanl' move origin (steps >>= expandStep)
+
+findIntersections :: [Map Cord Int] -> [Cord]
+findIntersections m = M.keys $ foldr M.intersection (head m) (tail m)
+
+solveA :: [[String]] -> Int
+solveA = minimum . fmap (manhaDist origin) . findIntersections . fmap makeWire
+
+solveB :: [[String]] -> Int
+solveB input =
+    minimum $ (\c -> sum $ fmap (M.! c) wires) <$> findIntersections wires
+    where wires = makeWire <$> input
 
 day03a :: String -> String
-day03a = id
+day03a = show . solveA . fmap (splitOn ",") . lines
 
 day03b :: String -> String
-day03b = id
+day03b = show . solveB . fmap (splitOn ",") . lines
--- src/Util.hs
+++ src/Util.hs
@@ -1,4 +1,4 @@
-module Util (strip, linesStrip, Data.List.Split.splitOn, mostFreq) where
+module Util (strip, linesStrip, Data.List.Split.splitOn, mostFreq, manhaDist) where
 
 import           Data.List.Split                ( splitOn )
 import qualified Data.Text                     as T
@@ -16,3 +16,6 @@ linesStrip = fmap (T.unpack . T.strip) . T.lines . T.strip . T.pack
 
 mostFreq :: Ord a => [a] -> [a]
 mostFreq = maximumBy (comparing length) . group . sort
+
+manhaDist :: (Int,Int) -> (Int,Int) -> Int
+manhaDist (x,y) (x',y') = abs (x - x') + abs (y - y')
