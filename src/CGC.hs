8ea779d074bf1d49ba4bf348b933970d6d4ed613
meck <johan@meck.se>
2019-12-09 21:05:42 +0100

    Day9

--- src/CGC.hs
+++ src/CGC.hs
@@ -15,9 +15,9 @@ where
 -- CGC: Christmas Guidance Computer!
 
 import           Control.Monad.State.Lazy
+import           Data.IntMap                    ( IntMap )
+import qualified Data.IntMap                   as IM
 import           Data.Maybe                     ( fromMaybe )
-import           Data.Vector                    ( Vector )
-import qualified Data.Vector                   as V
 import           Util                           ( digitsR )
 
 -- | The 'Effect' type contains all the primitive effects that are
@@ -29,10 +29,10 @@ data Effect a =
   | Fail String
 
 instance Functor Effect where
-    fmap f (Done     x) = Done (f x)
+    fmap f (Done    x) = Done (f x)
     fmap f (ReadInt t) = ReadInt (fmap f . t)
-    fmap f (Print c t ) = Print c (fmap f t)
-    fmap _ (Fail msg  ) = Fail msg
+    fmap f (Print c t) = Print c (fmap f t)
+    fmap _ (Fail msg ) = Fail msg
 
 instance Applicative Effect where
     pure  = Done
@@ -40,23 +40,23 @@ instance Applicative Effect where
 
 instance Monad Effect where
     return = Done
-    (Done     x) >>= f = f x
+    (Done    x) >>= f = f x
     (ReadInt t) >>= f = ReadInt (t >=> f)
-    (Print c t ) >>= f = Print c (t >>= f)
-    (Fail msg  ) >>= _ = Fail msg
+    (Print c t) >>= f = Print c (t >>= f)
+    (Fail msg ) >>= _ = Fail msg
 
 instance Eq a => Eq (Effect a) where
-    (Done x) == (Done y) = x == y
-    (ReadInt f) == (ReadInt g) =
-        all (\x -> f x == g x) [minBound .. maxBound]
+    (Done    x) == (Done    y) = x == y
+    (ReadInt f) == (ReadInt g) = all (\x -> f x == g x) [minBound .. maxBound]
     (Print c t) == (Print d u) = c == d && t == u
     (Fail s   ) == (Fail t   ) = s == t
     _           == _           = False
 
 type CGC a = StateT CGCState Effect a
 
-data CGCState = CGCState { mem      :: Vector Int,
-                           iP       :: Int } deriving (Eq, Show)
+data CGCState = CGCState { mem  :: IntMap Int,
+                           iP   :: Int,
+                           relB :: Int } deriving (Eq, Show)
 
 readInt :: CGC Int
 readInt = StateT (\s -> ReadInt (\c -> Done (c, s)))
@@ -67,66 +67,66 @@ printInt c = StateT (\s -> Print c (Done ((), s)))
 cgcFail :: String -> CGC a
 cgcFail str = StateT (\_ -> Fail str)
 
-newtype Pointer = Pointer { getPtr :: Int } deriving (Show, Eq)
-
 data Param = Ptr Int | Val Int deriving (Show, Eq)
 
-data Op = ADD Param Param Pointer
-        | MUL Param Param Pointer
-        | READ Pointer
+data Op = ADD Param Param Param
+        | MUL Param Param Param
+        | READ Param
         | WRITE Param
         | JZ Param Param
         | JNZ Param Param
-        | LESS Param Param Pointer
-        | CMP Param Param Pointer
+        | LESS Param Param Param
+        | CMP Param Param Param
+        | RBOFF Param
         | HALT
             deriving (Eq, Show)
 
 doOp :: Op -> CGC ()
 doOp (ADD x y r ) = mathOp (+) x y r
 doOp (MUL x y r ) = mathOp (*) x y r
-doOp (READ  d   ) = (readInt >>= setAdr d) >> moveIP 2
-doOp (WRITE m   ) = (derefPar m >>= printInt) >> moveIP 2
-doOp (JZ x d) = derefPar x >>= (\x' -> if x' == 0 then setIP d else moveIP 3)
-doOp (JNZ x d) = derefPar x >>= (\x' -> if x' /= 0 then setIP d else moveIP 3)
+doOp (READ  d   ) = (readInt >>= setAddr d) >> moveIP 2
+doOp (WRITE m   ) = (deref m >>= printInt) >> moveIP 2
+doOp (JZ  x d   ) = deref x >>= (\x' -> if x' == 0 then setIP d else moveIP 3)
+doOp (JNZ x d   ) = deref x >>= (\x' -> if x' /= 0 then setIP d else moveIP 3)
 doOp (LESS x y d) = do
-    x' <- derefPar x
-    y' <- derefPar y
-    if x' < y' then setAdr d 1 else setAdr d 0
+    x' <- deref x
+    y' <- deref y
+    if x' < y' then setAddr d 1 else setAddr d 0
     moveIP 4
 doOp (CMP x y d) = do
-    x' <- derefPar x
-    y' <- derefPar y
-    if x' == y' then setAdr d 1 else setAdr d 0
+    x' <- deref x
+    y' <- deref y
+    if x' == y' then setAddr d 1 else setAddr d 0
     moveIP 4
+doOp (RBOFF x) = do
+    s <- get
+    v <- deref x
+    put $ s { relB = relB s + v }
+    moveIP 2
 doOp HALT = pure ()
 
 -- Helpers
-mathOp :: (Int -> Int -> Int) -> Param -> Param -> Pointer -> CGC ()
-mathOp f x y d = do
-    x' <- derefPar x
-    y' <- derefPar y
-    setAdr d (x' `f` y')
+mathOp :: (Int -> Int -> Int) -> Param -> Param -> Param -> CGC ()
+mathOp _ _ _ (Val _) = cgcFail "Pointer Parameter not allowed"
+mathOp f x y d       = do
+    x' <- deref x
+    y' <- deref y
+    setAddr d (x' `f` y')
     moveIP 4
 
-derefPar :: Param -> CGC Int
-derefPar (Val i) = pure i
-derefPar (Ptr p) = derefPtr (Pointer p)
+deref :: Param -> CGC Int
+deref (Val i   ) = pure i
+deref (Ptr addr) = if addr < 0
+    then cgcFail "Accessing negative mem addr"
+    else IM.findWithDefault 0 addr . mem <$> get
 
-derefPtr :: Pointer -> CGC Int
-derefPtr (Pointer addr) = do
-    st <- get
-    let mi = mem st V.!? addr
-    case mi of
-        Just i  -> pure i
-        Nothing -> cgcFail "Memory access out of bounds"
-
-setAdr :: Pointer -> Int -> CGC ()
-setAdr (Pointer addr) val = modify $ \s -> s { mem = mem s V.// [(addr, val)] }
+setAddr :: Param -> Int -> CGC ()
+setAddr (Val _   ) _   = cgcFail "DMA not allowed"
+setAddr (Ptr addr) val = modify $ \s -> s { mem = IM.insert addr val (mem s) }
 
 setIP :: Param -> CGC ()
 setIP i = do
-    i' <- derefPar i
+    i' <- deref i
     s  <- get
     put $ s { iP = i' }
 
@@ -136,32 +136,34 @@ moveIP i = modify $ \s -> s { iP = iP s + i }
 
 nextInst :: CGCState -> Op
 nextInst cgc =
-    let op  = fromMaybe (error "CGC Error: IP out of bounds") $ m V.!? i
-        pa0 = par 0
-        pa1 = par 1
-        po0 = ptr 0
-        po2 = ptr 2
-    in  case digPad 2 op of
-            [1, 0] -> ADD pa0 pa1 po2
-            [2, 0] -> MUL pa0 pa1 po2
-            [3, 0] -> READ po0
-            [4, 0] -> WRITE pa0
-            [5, 0] -> JNZ pa0 pa1
-            [6, 0] -> JZ pa0 pa1
-            [7, 0] -> LESS pa0 pa1 po2
-            [8, 0] -> CMP pa0 pa1 po2
+    let opCode = fromMaybe (error "CGC Error: IP out of bounds") $ m IM.!? ip
+        p0     = parsePara 0
+        p1     = parsePara 1
+        p2     = parsePara 2
+    in  case digPad 2 opCode of
+            [1, 0] -> ADD p0 p1 p2
+            [2, 0] -> MUL p0 p1 p2
+            [3, 0] -> READ p0
+            [4, 0] -> WRITE p0
+            [5, 0] -> JNZ p0 p1
+            [6, 0] -> JZ p0 p1
+            [7, 0] -> LESS p0 p1 p2
+            [8, 0] -> CMP p0 p1 p2
+            [9, 0] -> RBOFF p0
             [9, 9] -> HALT
-            _      -> error $ "Invalid CGC  Instruction: " <> show op
+            _      -> error $ "Invalid CGC  Instruction: " <> show opCode
   where
-    par n = fromMaybe (error "Parameter error") $ do
-        t <- last . digPad (n + 3) <$> (m V.!? i)
-        case t of
-            0 -> Ptr <$> (m V.!? (succ i + n))
-            1 -> Val <$> (m V.!? (succ i + n))
-            _ -> Nothing
-    ptr n = maybe (error "Parameter error") Pointer (m V.!? (succ i + n))
-    i = iP cgc
-    m = mem cgc
+    parsePara n =
+        let paramAddr = succ ip + n
+        in  fromMaybe (error "Parameter error") $ do
+                t <- last . digPad (n + 3) <$> (m IM.!? ip)
+                case t of
+                    0 -> Ptr <$> (m IM.!? paramAddr)
+                    1 -> Val <$> (m IM.!? paramAddr)
+                    2 -> Ptr . (+) (relB cgc) <$> (m IM.!? paramAddr)
+                    _ -> Nothing
+    ip = iP cgc
+    m  = mem cgc
     digPad n int = take n $ digitsR int <> repeat 0
 
 -- API
@@ -181,7 +183,7 @@ evalCGC s inp = case runCGC s (inp, []) of
     Halted  ((_, o), _) -> o
     Waiting _           -> error "CGC Error: No input"
 
-type Buffers = ([Int],[Int])
+type Buffers = ([Int], [Int])
 
 data Result = Halted (Buffers,CGCState) | Waiting (Buffers, CGCState)
 
@@ -198,12 +200,13 @@ runCGC s buf = handleEff buf $ runStateT (get >>= doOp . nextInst) s
 
 -- Setup with memory
 initCGC :: [Int] -> CGCState
-initCGC m = CGCState (V.fromList m) 0
+initCGC m = CGCState (IM.fromList (zip [0 ..] m)) 0 0
 
 -- Set the Verb and Noun
 setVerbAndNoun :: (Int, Int) -> (CGCState -> CGCState)
-setVerbAndNoun (v, n) cgc = cgc { mem = mem cgc V.// [(1, v), (2, n)] }
+setVerbAndNoun (v, n) cgc =
+    cgc { mem = IM.union (IM.fromList [(1, v), (2, n)]) (mem cgc) }
 
 -- Get the memory
 readMem :: CGCState -> [Int]
-readMem cgc = V.toList $ mem cgc
+readMem = fmap snd . IM.toAscList . mem
--- src/Day9.hs
+++ src/Day9.hs
@@ -1,8 +1,10 @@
 module Day9 (day09a, day09b) where
 
+import           Data.List.Split                ( splitOn )
+import           CGC
 
 day09a :: String -> String
-day09a = id
+day09a = show . head . flip evalCGC [1] . initCGC . fmap read . splitOn ","
 
 day09b :: String -> String
-day09b = id
+day09b = show . head . flip evalCGC [2] . initCGC . fmap read . splitOn ","
