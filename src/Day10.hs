2c156ab42be9f17f143eb8c2b801f2be510f8592
meck <johan@meck.se>
2019-12-11 11:49:30 +0100

    Day 10 done

--- src/Day10.hs
+++ src/Day10.hs
@@ -1,67 +1,43 @@
 module Day10 (day10a, day10b) where
 
 import           Data.List                      ( sortOn
-                                                , unfoldr
-                                                , groupBy
                                                 , transpose
-                                                , sort
                                                 )
-import           Data.Maybe                     ( mapMaybe )
-import           Data.Ratio
 import           Data.Set                       ( Set )
 import qualified Data.Set                      as S
-import           Data.Function                  ( on )
-import           Util                           ( bimap'
-                                                , strip
+import           Util                           ( groupSortOn
                                                 , manhaDist
-                                                , (-^)
                                                 )
 
 type Cord = (Integer, Integer)
 
 type AstMap = Set Cord
 
--- The n'th order Farey sequence.
-farey :: Integer -> [Rational]
-farey n = 0 : unfoldr step (0, 1, 1, n)
+-- Get the angle between A B (0 to 2pi) 0 north
+angle :: Cord -> Cord -> Double
+angle (xA, yA) (xB, yB) = (\a -> if a < 0 then a + 2 * pi else a) $ atan2 dx dy
   where
-    step (a, b, c, d)
-        | c > n
-        = Nothing
-        | otherwise
-        = let k = (n + b) `quot` d
-          in  Just (c % d, (c, d, k * c - a, k * d - b))
+    dx = fromInteger $ xB - xA
+    dy = fromInteger $ yA - yB
 
--- The point on a line a fraction `k` of the length
-rationalPoint :: Num b => (b, b) -> (b, b) -> b -> (b, b)
-rationalPoint (ax, ay) (bx, by) k = (ax + k * (bx - ax), ay + k * (by - ay))
-
--- All Integer points berween two points
-interCords :: Cord -> Cord -> [Cord]
-interCords a b =
-    mapMaybe intCord $ 
-    rationalPoint (bimap' toRational a) (bimap' toRational b)
-        <$> frac
-  where
-    -- The number of segments of a line
-    nSeg      = uncurry gcd $ a -^ b
-    -- The fractions of those intersections
-    -- excluding (0/1) and (1/1)
-    frac      = filter (not . isInteger) $ farey nSeg
-    isInteger = (1 ==) . denominator
-    intCord c = if uncurry (&&) $ bimap' isInteger c
-        then Just $ bimap' numerator c
-        else Nothing
-
-losClear :: AstMap -> Cord -> Cord -> Bool
-losClear _ x y | x == y = False
-losClear m x y          = not $ or $ flip S.member m <$> interCords x y
-
+-- Num of seen astoroids from a point
+-- using fusion of the set
 noOfseen :: AstMap -> Cord -> Int
-noOfseen m c = length $ filter id $ fmap (losClear m c) (S.toList m)
+noOfseen am c = S.size $ S.map (angle c) am
 
-findBest :: AstMap -> (Cord, Int)
-findBest m = last $ sortOn snd $ zip l (noOfseen m <$> l) where l = S.toList m
+-- Cord with most visible astoroids
+findBest :: AstMap -> (Int, Cord)
+findBest m = S.findMax $ S.map ((,) =<< noOfseen m) m
+
+-- The list of vaporizing
+vapeList :: Cord -> AstMap -> [Cord]
+vapeList c =
+    concat
+        . transpose
+        . fmap (sortOn (manhaDist c))
+        . groupSortOn (angle c)
+        . filter (/= c)
+        . S.toList
 
 parse :: String -> AstMap
 parse inp = S.fromList $ snd <$> filter ((== '#') . fst) cord
@@ -72,30 +48,10 @@ parse inp = S.fromList $ snd <$> filter ((== '#') . fst) cord
                [ (x, y) | y <- [0 .. pred h], x <- [0 .. pred w] ]
 
 day10a :: String -> String
-day10a = show . snd . findBest . parse
-
-
-test am c@(cx,cy) =  e
-    where a = groupOn ang $ filter (/=c) $ S.toList am
-          b = sortOn (manhaDist c) <$> a
-          d = sortOn (ang . head) b
-          -- d = sortOn ( (\ang' -> if ang' < pi/2 then ang' - 2 *pi else ang') . ang . head) b
-          e = concat $ transpose d
-          ang = (\ang' -> if ang' < 0 then ang' + 2*pi else ang') . uncurry  atan2 . bimap' fromInteger . (\(ax,ay) -> (ax - cx, cy - ay))
-          -- ang = uncurry  atan2 . bimap' fromInteger . (\(ax,ay) -> ( ax - cx ,cy - ay ))
-          -- ang = uncurry atan2 . bimap' fromInteger . (-^ c)
-          dist (ax,ay) (bx,by) = sqrt $ (fromInteger ax - fromInteger bx)**2 + (fromInteger ay - fromInteger by)**2
-
--- | A version of 'group' where the equality is done on some extracted value.
-groupOn :: Eq b => (a -> b) -> [a] -> [[a]]
-groupOn f = groupBy ((==) `on2` f)
--- groupOn f = groupBy ((==) `on2` f)
-    -- redefine on so we avoid duplicate computation for most values.
-    where (.*.) `on2` f = \x -> let fx = f x in \y -> fx .*. f y
-
--- ang' = uncurry atan2 . bimap' fromInteger . ((2,3) -^)
+day10a = show . fst . findBest . parse
 
 day10b :: String -> String
--- day10b = id
-day10b inp = show $ test p (fst $ findBest p)
-    where p = parse $ strip $ inp
+day10b inp = show $ uncurry ((+) . (100 *)) $ (!! 199) $ vapeList base astMap
+  where
+    astMap = parse inp
+    base   = snd $ findBest astMap
--- src/Util.hs
+++ src/Util.hs
@@ -7,16 +7,30 @@ module Util
     , manhaDist
     , digitsR
     , digits
+    , Data.Bifunctor.bimap
+    , bimap'
+    , (-^)
+    , (+^)
+    , groupSortOn
     )
 where
 
+import           Control.Arrow                  ( (&&&) )
+import           Data.Bifunctor
+import           Data.Function                  ( on )
 import           Data.List                      ( group
+                                                , groupBy
                                                 , maximumBy
                                                 , sort
+                                                , sortBy
                                                 )
 import           Data.List.Split                ( splitOn )
 import           Data.Ord                       ( comparing )
 import qualified Data.Text                     as T
+
+
+bimap' :: Bifunctor p => (a -> d) -> p a a -> p d d
+bimap' f = bimap f f
 
 strip :: String -> String
 strip = T.unpack . T.strip . T.pack
@@ -32,8 +46,18 @@ manhaDist (x, y) (x', y') = abs (x - x') + abs (y - y')
 
 digitsR :: Integral n => n -> [n]
 digitsR 0 = []
-digitsR x =
-    let (rest, lastDigit) = quotRem x 10 in lastDigit : digitsR rest
+digitsR x = let (rest, lastDigit) = quotRem x 10 in lastDigit : digitsR rest
 
 digits :: Integral n => n -> [n]
 digits = reverse . digitsR
+
+(+^) :: (Num a, Num b) => (a, b) -> (a, b) -> (a, b)
+(+^) (ax, ay) (bx, by) = (ax + bx, ay + by)
+
+(-^) :: (Num a, Num b) => (a, b) -> (a, b) -> (a, b)
+(-^) (ax, ay) (bx, by) = (ax - bx, ay - by)
+
+groupSortOn :: Ord b => (a -> b) -> [a] -> [[a]]
+groupSortOn f =
+    map (map snd) . groupBy ((==) `on` fst) . sortBy (compare `on` fst) . map
+        (f &&& id)
